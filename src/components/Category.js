b7a228d03bee48e6445e9dd6ef409ed198e0d516
A-BMT02 <30394037+A-BMT02@users.noreply.github.com>
2022-10-27 20:35:32 +0300

    category-responsive

--- src/components/Category.js
+++ src/components/Category.js
@@ -67,7 +67,10 @@ export const Category = () => {
   return (
     <div className="flex flex-col space-y-6 mt-10 relative">
       <div className="absolute top-0 -left-36">
-        <img src={circle2} />
+        <img
+          className="w-200px max-w-[200px] md:w-none md:max-w-none"
+          src={circle2}
+        />
       </div>
       <div className="flex flex-col space-y-6 items-center mt-10 w-full">
         <div className="flex flex-col space-y-1">
@@ -75,15 +78,15 @@ export const Category = () => {
           <div className="bg-opink w-[50px] h-[8px] rounded-xl "></div>
         </div>
         <div className="relative w-full ">
-          <div className="h-[450px] max-h-[450px] flex justify-center">
+          <div className="h-[300px] md:h-[450px] md:max-h-[450px] max-h-[300px] md:h-[450px] md:max-h-[450px] flex justify-center">
             <img
               src={outwear}
-              className="h-[450px] max-h-[450px] object-cover w-4/5 "
+              className="h-[300px] md:h-[450px] md:max-h-[450px] max-h-[300px] md:h-[450px] md:max-h-[450px] object-cover w-4/5 "
             />
           </div>
           <div className="absolute top-1/2 -translate-x-2/4 -translate-y-2/4 left-1/2 flex flex-col justify-center items-center space-y-6">
-            <p className="text-white text-3xl font-bold">Outwear</p>
-            <div className="cursor-pointer flex space-x-6 items-center bg-opink px-3 py-2 pl-8 text-white rounded-3xl">
+            <p className="text-white text-2xl md:text-3xl font-bold">Outwear</p>
+            <div className="cursor-pointer flex space-x-4 md:space-x-6 items-center bg-opink px-2 md:px-3 py-1 md:py-2 pl-4 md:pl-8 text-white rounded-3xl">
               <p className="text-">SEE MORE</p>
               <img className="cursor-pointer rotate-180 w-10" src={back} />
             </div>
@@ -91,9 +94,9 @@ export const Category = () => {
         </div>
       </div>
 
-      <div className="flex justify-center space-x-10 items-center px-10">
+      <div className="flex justify-center space-x-4 md:space-x-10 items-center px-5 md:px-10">
         <div onClick={(e) => previous()}>
-          <img className="cursor-pointer" src={back} />
+          <img className="cursor-pointer w-[25px] md:w-none" src={back} />
         </div>
         <div className="w-full overflow-hidden">
           <div
@@ -102,8 +105,23 @@ export const Category = () => {
             style={{ width: `${200}%` }}
           >
             {/* first */}
-            <div className="flex justify-center space-x-10 items-center w-full">
-              <div className="flex flex-col space-y-4 border border-opink p-4 rounded-lg">
+            <div className="flex justify-center space-x-4 md:space-x-10 items-center w-full">
+              <div className="hidden md:flex flex-col space-y-4 border border-opink p-2 md:p-4 rounded-lg">
+                <div className="">
+                  <img className="" src={girl4} />
+                </div>
+                <div className="flex justify-between">
+                  <div className="flex flex-col space-y-2">
+                    <p className="text-tgray">Loose Knit 3/4 Sleeve</p>
+                    <div className="flex space-x-2">
+                      <p className="text-opink">$119.99</p>
+                      <del className="text-tgray">$149.99</del>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex flex-col space-y-4 border border-opink p-2 md:p-4 rounded-lg">
                 <div className="">
                   <img src={girl4} />
                 </div>
@@ -118,7 +136,7 @@ export const Category = () => {
                 </div>
               </div>
 
-              <div className="flex flex-col space-y-4 border border-opink p-4 rounded-lg">
+              <div className="hidden lg:flex flex-col space-y-4 border border-opink p-4 rounded-lg">
                 <div className="">
                   <img src={girl4} />
                 </div>
@@ -133,7 +151,7 @@ export const Category = () => {
                 </div>
               </div>
 
-              <div className="flex flex-col space-y-4 border border-opink p-4 rounded-lg">
+              <div className="hidden xl:flex flex-col space-y-4 border border-opink p-4 rounded-lg">
                 <div className="">
                   <img src={girl4} />
                 </div>
@@ -148,22 +166,7 @@ export const Category = () => {
                 </div>
               </div>
 
-              <div className="flex flex-col space-y-4 border border-opink p-4 rounded-lg">
-                <div className="">
-                  <img src={girl4} />
-                </div>
-                <div className="flex justify-between">
-                  <div className="flex flex-col space-y-2">
-                    <p className="text-tgray">Loose Knit 3/4 Sleeve</p>
-                    <div className="flex space-x-2">
-                      <p className="text-opink">$119.99</p>
-                      <del className="text-tgray">$149.99</del>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="flex flex-col space-y-4 border border-opink p-4 rounded-lg">
+              <div className="hidden md:flex flex-col space-y-4 border border-opink p-4 rounded-lg">
                 <div className="">
                   <img src={girl4} />
                 </div>
@@ -180,8 +183,8 @@ export const Category = () => {
             </div>
 
             {/* second */}
-            <div className="flex justify-center space-x-10 items-center w-full">
-              <div className="flex flex-col space-y-4 border border-opink p-4 rounded-lg">
+            <div className="flex justify-center space-x-4 md:space-x-10 items-center w-full">
+              <div className="flex flex-col space-y-4 border border-opink p-2 md:p-4 rounded-lg">
                 <div className="">
                   <img src={girl4} />
                 </div>
@@ -196,7 +199,7 @@ export const Category = () => {
                 </div>
               </div>
 
-              <div className="flex flex-col space-y-4 border border-opink p-4 rounded-lg">
+              <div className="hidden md:flex flex-col space-y-4 border border-opink p-2 md:p-4 rounded-lg">
                 <div className="">
                   <img src={girl4} />
                 </div>
@@ -211,7 +214,7 @@ export const Category = () => {
                 </div>
               </div>
 
-              <div className="flex flex-col space-y-4 border border-opink p-4 rounded-lg">
+              <div className="hidden lg:flex flex-col space-y-4 border border-opink p-4 rounded-lg">
                 <div className="">
                   <img src={girl4} />
                 </div>
@@ -226,7 +229,7 @@ export const Category = () => {
                 </div>
               </div>
 
-              <div className="flex flex-col space-y-4 border border-opink p-4 rounded-lg">
+              <div className="hidden xl:flex flex-col space-y-4 border border-opink p-4 rounded-lg">
                 <div className="">
                   <img src={girl4} />
                 </div>
@@ -241,7 +244,7 @@ export const Category = () => {
                 </div>
               </div>
 
-              <div className="flex flex-col space-y-4 border border-opink p-4 rounded-lg">
+              <div className="hidden md:flex flex-col space-y-4 border border-opink p-4 rounded-lg">
                 <div className="">
                   <img src={girl4} />
                 </div>
@@ -260,16 +263,19 @@ export const Category = () => {
         </div>
 
         <div onClick={(e) => next()}>
-          <img className="rotate-180 cursor-pointer" src={back} />
+          <img
+            className="rotate-180 cursor-pointer w-[25px] md:w-none"
+            src={back}
+          />
         </div>
       </div>
 
       <div className="flex flex-col space-y-10 bg-opink px-5  py-14 items-center">
-        <p className="text-white text-3xl self-start ml-20">
+        <p className="text-white text-2xl md:text-3xl  text-center md:self-start md:ml-20">
           The world's finest models
         </p>
 
-        <div className="flex justify-center space-x-10 items-center px-10 w-full">
+        <div className="flex justify-center space-x-4 md:space-x-10 items-center  md:px-10 w-full">
           <div onClick={(e) => previous2()}>
             <img className="cursor-pointer" src={back} />
           </div>
@@ -280,7 +286,7 @@ export const Category = () => {
               style={{ width: `${200}%` }}
             >
               {/* first */}
-              <div className="flex justify-center space-x-10 items-center w-full">
+              <div className="flex justify-center space-x-4 md:space-x-10 items-center w-full">
                 <div className="flex flex-col space-y-4 bg-white p-4 ">
                   <div className="border border-opink p-4 rounded-lg">
                     <img src={girl5} />
@@ -296,7 +302,7 @@ export const Category = () => {
                   </div>
                 </div>
 
-                <div className="flex flex-col space-y-4 bg-white p-4 ">
+                <div className="hidden md:flex flex-col space-y-4 bg-white p-4 ">
                   <div className="border border-opink p-4 rounded-lg">
                     <img src={girl5} />
                   </div>
@@ -311,7 +317,7 @@ export const Category = () => {
                   </div>
                 </div>
 
-                <div className="flex flex-col space-y-4 bg-white p-4 ">
+                <div className="hidden md:flex flex-col space-y-4 bg-white p-4 ">
                   <div className="border border-opink p-4 rounded-lg">
                     <img src={girl5} />
                   </div>
@@ -326,7 +332,7 @@ export const Category = () => {
                   </div>
                 </div>
 
-                <div className="flex flex-col space-y-4 bg-white p-4 ">
+                <div className="hidden md:flex flex-col space-y-4 bg-white p-4 ">
                   <div className="border border-opink p-4 rounded-lg">
                     <img src={girl5} />
                   </div>
@@ -341,7 +347,7 @@ export const Category = () => {
                   </div>
                 </div>
 
-                <div className="flex flex-col space-y-4 bg-white p-4 ">
+                <div className="hidden md:flex flex-col space-y-4 bg-white p-4 ">
                   <div className="border border-opink p-4 rounded-lg">
                     <img src={girl5} />
                   </div>
@@ -357,7 +363,7 @@ export const Category = () => {
                 </div>
               </div>
               {/* second */}
-              <div className="flex justify-center space-x-10 items-center w-full">
+              <div className="flex justify-center space-x-4 md:space-x-10 items-center w-full">
                 <div className="flex flex-col space-y-4 bg-white p-4 ">
                   <div className="border border-opink p-4 rounded-lg">
                     <img src={girl5} />
@@ -373,7 +379,7 @@ export const Category = () => {
                   </div>
                 </div>
 
-                <div className="flex flex-col space-y-4 bg-white p-4 ">
+                <div className="hidden md:flex flex-col space-y-4 bg-white p-4 ">
                   <div className="border border-opink p-4 rounded-lg">
                     <img src={girl5} />
                   </div>
@@ -388,7 +394,7 @@ export const Category = () => {
                   </div>
                 </div>
 
-                <div className="flex flex-col space-y-4 bg-white p-4 ">
+                <div className="hidden md:flex flex-col space-y-4 bg-white p-4 ">
                   <div className="border border-opink p-4 rounded-lg">
                     <img src={girl5} />
                   </div>
@@ -403,7 +409,7 @@ export const Category = () => {
                   </div>
                 </div>
 
-                <div className="flex flex-col space-y-4 bg-white p-4 ">
+                <div className="hidden md:flex flex-col space-y-4 bg-white p-4 ">
                   <div className="border border-opink p-4 rounded-lg">
                     <img src={girl5} />
                   </div>
@@ -418,7 +424,7 @@ export const Category = () => {
                   </div>
                 </div>
 
-                <div className="flex flex-col space-y-4 bg-white p-4 ">
+                <div className="hidden md:flex flex-col space-y-4 bg-white p-4 ">
                   <div className="border border-opink p-4 rounded-lg">
                     <img src={girl5} />
                   </div>
